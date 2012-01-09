c1b39fea436c710484f2c7ff9d5892d451c314ae
Henrik Sandklef <hesa@sandklef.com>
2012-01-09 22:55:18 +0100

    Adding fault tolerance to build/test script

--- bin/get-arduino-src.sh
+++ bin/get-arduino-src.sh
@@ -6,7 +6,6 @@ then
     exit 0 
 fi
 
-
 MY_OS=$(uname -s)
 
 if [ "$MY_OS" = "Linux" ]
@@ -26,31 +25,59 @@ else
     exit 1
 fi
 
+exit_on_failure()
+{
+    RET=$1
+    shift
+    COMM="$*"
+    if [ "$RET" != "0" ]
+	then
+	echo "***************************"
+	echo "****  ERROR in $0:  $COMM"
+	echo "***************************"
+	exit $RET
+    fi
+}
+
+exec_comm()
+{
+    echo "$*"
+    $*
+    exit_on_failure "$?" "$*"
+
+#    echo "Finished: $*"
+#    sleep 2
+}
+
+
+
+
 get_sources()
 {
-    mkdir -p download-tmp
-    rm -f $ARD_FILE
-    wget $ARD_URL
-    mv   $ARD_FILE download-tmp
+    exec_comm mkdir -p download-tmp
+    exec_comm rm -f $ARD_FILE
+#    wget $ARD_URL
+    exec_comm cp /tmp/arduino-1**.tgz .
+    exec_comm mv   $ARD_FILE download-tmp
 }
 
 unpack_sources()
 {
-    cd download-tmp
-    tar zxvf $ARD_FILE
-    cd ..
+    exec_comm cd download-tmp
+    exec_comm tar zxvf $ARD_FILE
+    exec_comm cd ..
 }
 
 setup_sources()
 {
-    mkdir arduino-sources
-    mkdir arduino-sources/core
-    mkdir arduino-sources/variants
+    exec_comm mkdir arduino-sources
+    exec_comm mkdir arduino-sources/core
+    exec_comm mkdir arduino-sources/variants
 
-    cp -r download-tmp/arduino-1.0/hardware/arduino/cores/arduino/* arduino-sources/core
-    cp -r download-tmp/arduino-1.0/hardware/arduino/variants/* arduino-sources/variants/
+    exec_comm cp -r download-tmp/arduino-1.0/hardware/arduino/cores/arduino/* arduino-sources/core
+    exec_comm cp -r download-tmp/arduino-1.0/hardware/arduino/variants/* arduino-sources/variants/
 
-    cp misc/Makefile-arduino-sources  arduino-sources/Makefile
+    exec_comm cp misc/Makefile-arduino-sources  arduino-sources/Makefile
 }
 
 get_sources
