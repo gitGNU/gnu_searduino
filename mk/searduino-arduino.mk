60380e1fc0da8ab9890343498a81061299f551e3
Henrik Sandklef <hesa@sandklef.com>
2012-01-19 23:22:11 +0100

    First rev

---
+++ mk/searduino-arduino.mk
@@ -0,0 +1,65 @@
+ifeq (${ARDUINO},due)
+BOARD=atmega328
+CPU=atmega328p
+VARIANT=standard
+ARDUINO_CPU=ATmega328P
+USB_DEV=/dev/ttyUSB0
+endif
+ifeq ($(ARDUINO),uno)
+BOARD=atmega328
+CPU=atmega328p
+VARIANT=standard
+ARDUINO_CPU=ATmega328p
+USB_DEV=/dev/ttyACM0
+endif
+ifeq ($(ARDUINO),mega)
+BOARD=atmega1280
+CPU=atmega1280
+VARIANT=mega
+ARDUINO_CPU=ATmega1280
+USB_DEV=/dev/ttyUSB0
+endif
+
+include $(SEARDUINO_PATH)/mk/board-makefiles/$(BOARD).mk
+
+
+
+
+OBJ_C = $(SRC_C:.c=.o) 
+OBJ_CXX = $(SRC_CXX:.cpp=.o) 
+
+
+CC=avr-gcc
+CXX=avr-g++
+OBJ_CP=avr-objcopy
+AR=avr-ar
+AVRDUDE=avrdude
+F_CPU=$(board_build.f_cpu)
+
+INC_FLAGS=  -I$(SEARDUINO_PATH)/arduino-sources/core \
+            -I$(SEARDUINO_PATH)/arduino-sources/variants/$(VARIANT) 
+
+LIBSEARDUINO_C_CPP_FLAGS= -g -Os -w -fno-exceptions \
+                          -ffunction-sections -fdata-sections \
+                          -mmcu=$(CPU) -DF_CPU=$(F_CPU) \
+                          -DARDUINO=$(ARDUINO_VERSION) \
+                           $(INC_FLAGS)
+
+$(MAIN_SRC).elf: $(MAIN_SRC).o  $(OBJ_C) $(OBJ_CXX)
+	$(CC) -Os -Wl,--gc-sections -mmcu=$(CPU)  -o $(MAIN_SRC).elf $(MAIN_SRC).o $(LIB) -lm $(LDFLAGS) $(OBJ_C) $(OBJ_CXX)
+
+$(MAIN_SRC).o: $(MAIN_SRC) 
+	$(CC) -c $(CFLAGS)  $(MAIN_SRC) -o  $(MAIN_SRC).o
+
+hex: $(MAIN_SRC).hex
+$(MAIN_SRC).hex:   $(MAIN_SRC).o $(LIB) $(MAIN_SRC).elf
+	$(OBJ_CP)   -O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma  .eeprom=0 $(MAIN_SRC).elf $(MAIN_SRC).eep 
+	$(OBJ_CP)  -O ihex -R .eeprom $(MAIN_SRC).elf $(MAIN_SRC).hex  
+
+$(PROG): $(MAIN_SRC).hex 
+	@echo "--- Program '$(PROG).hex' ready for upload ---"
+
+upload: $(MAIN_SRC).hex
+	echo "$(ARDUINO)   $(BOARD)"
+	$(AVRDUDE) -q -q -p$(CPU) -carduino -P$(USB_DEV) -b$(board_upload.speed) -D -Uflash:w:${MAIN_SRC}.hex:i
+
---
+++ mk/searduino-stub.mk
@@ -0,0 +1,34 @@
+BOARD=stub
+
+E_C = $(SRC_C:.c=.E) $(MAIN_SRC:.c=.o)
+E_CXX = $(SRC_CXX:.cpp=.E) 
+OBJ_C = $(SRC_C:.c=.o) $(MAIN_SRC:.c=.o)
+OBJ_CXX = $(SRC_CXX:.cpp=.o) 
+
+CC=gcc
+CXX=g++
+OBJ_CP=
+AVRDUDE=
+AR=ar
+JAVAC=gcj
+
+INC_FLAGS= -I$(SEARDUINO_PATH)/arduino-sources/core \
+           -I$(SEARDUINO_PATH)/faked-arduino/include/arduino \
+           -I$(SEARDUINO_PATH)/faked-arduino/include
+
+SEARDUINO_LIB_PATH= $(SEARDUINO_PATH)/libs
+
+_CFLAGS=   -g $(USER_C_FLAGS) -Wall \
+           $(LIB_FLAGS) \
+           $(MODULE_C_FLAGS) \
+           $(INC_FLAGS)
+
+_CXXFLAGS=-g $(USER_CXX_FLAGS) \
+            $(MODULE_CXX_FLAGS) \
+           $(INC_FLAGS)
+
+_LDFLAGS = $(USER_LD_FLAGS) -lpthread 
+
+$(PROG): $(LIB) $(OBJ_C) $(OBJ_CXX)
+	echo "OBJ_C  $(OBJ_C)"
+	$(CC) $(LIB) $(OBJ_C) $(OBJ_CXX) -o $(PROG) $(LDFLAGS) 
