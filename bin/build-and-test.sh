f6024933ebc6bf326b772db3cb219f2273c563bf
Henrik Sandklef <hesa@sandklef.com>
2012-01-22 00:17:44 +0100

    Debug printouts

--- bin/build-and-test.sh
+++ bin/build-and-test.sh
@@ -6,13 +6,22 @@ TMP_INST=/tmp/TMP_INSTALL3
 rm -fr /tmp/TMP_INSTALL3/ && \
 make -f Makefile.git      && \
 ./configure --prefix=${TMP_INST} && \
-make && \
-cd example/ && make && make install && cd .. && \
- cd mk/ && make && make install && cd .. && \
-cd arduino-sources/ && make && make install && cd .. && \
-cd extensions/ && make && make install && cd .. && \
-cd test/ && cd hw && make && make check && cd ../.. && \
-cd test/ && cd shared && make && make check && cd ../.. && \
-cd test/ && cd shared-main && make && make check && cd ../.. && \
-cd test/ && cd stub && make && make check && cd ../..  && \
-pushd ${TMP_INST}/share/searduino/example/digpins/ && make -f Makefile.digpins clean all && popd
+make clean && make && \
+cd mk/ && make clean && make && make install && cd .. && \
+cd arduino-sources/ && make clean && make && make install && cd .. && \
+cd faked-arduino/ && make clean && make && make install && cd .. && \
+cd extensions/ && make clean && make && make install && cd .. && \
+cd example/ && make clean && make && make install && cd .. && \
+echo "Test 1 in vcs dir " && \
+cd test/ && cd hw && make clean && make && make check && cd ../.. && \
+echo "Test 2 in vcs dir " && \
+cd test/ && cd shared && make clean && make && make check && cd ../.. && \
+echo "Test 3 in vcs dir " && \
+cd test/ && cd shared-main && make clean && make && make check && cd ../.. && \
+echo "Test 4 in vcs dir " && \
+cd test/ && cd stub && make clean && make && make check && cd ../..  && \
+echo "TEST 1 in dest dir" && \
+pushd ${TMP_INST}/share/searduino/example/digpins/ && make -f Makefile.digpins clean all && popd && \
+echo "TEST 2 in dest dir" && \
+pushd ${TMP_INST}/share/searduino/example/python-digcounter && make -f Makefile.digcounter clean all && popd \
+echo "Done"
