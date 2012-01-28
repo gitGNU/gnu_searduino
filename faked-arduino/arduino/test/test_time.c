f3049a6419bf4831657b58606f4de0c26b47450a
Henrik Sandklef <hesa@sandklef.com>
2012-01-28 15:47:26 +0100

    Added tst code

--- faked-arduino/arduino/test/test_time.c
+++ faked-arduino/arduino/test/test_time.c
@@ -21,20 +21,29 @@
  * MA  02110-1301, USA.                                              
  ****/
 
-#include "time_stuff.h"
+
+unsigned long 
+micros(void);
+
+
+
+//#include "time_stuff.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 int fail_ctr=0;
 int succ_ctr=0;
-#define DELAY1 10000
-#define DELAY2 100000
+#define DELAY1 1000*10
+#define DELAY2 1000*100
+
+#define MS_DELAY1 10
+#define MS_DELAY2 100
 
 void test_micros(void)
 {
   int i ;
   unsigned long micros_since_start;
-  unsigned long expected_time;
+  unsigned long expected_time = 0;
   unsigned long prev_rounds_time;
   int percentage;
   double diff;
@@ -42,48 +51,83 @@ void test_micros(void)
   init_time();
 
 
-  for (i=1;i<200;i++)
+  for (i=1;i<5;i++)
     {
       usleep (DELAY1);
+
       micros_since_start = micros();
       expected_time +=  DELAY1;
 
-      diff = (double)labs(micros_since_start - expected_time)  /
-	(double)micros_since_start ;
-      percentage = (int)(diff * 100);
+      diff = labs(micros_since_start - expected_time);
 
-      if (percentage < 3)
+      percentage = (diff ) / DELAY1;
+
+      if (percentage<20)
 	{
 	  succ_ctr++;
       }
       else
 	{
-	  printf ("Fail 1....\n");
+	  printf ("Fail 1.... %d\n", percentage);
 	  fail_ctr++;
 	}
     }
 
 
-  for (i=1;i<20;i++)
+  for (i=1;i<5;i++)
     {
       usleep (DELAY2);
       micros_since_start = micros();
 
       expected_time +=  DELAY2;
 
-      diff = (double)labs(micros_since_start - expected_time)  /
-	(double)micros_since_start ;
-      percentage = (int)(diff * 100);
+      diff = micros_since_start - expected_time ;
 
-      if (percentage < 3)
+      percentage = ( diff ) / DELAY2;
+
+      if (percentage < 50)
 	{
 	  succ_ctr++;
       }
       else
 	{
-	  printf ("Fail 2....%d \n", percentage);
+	  printf ("Fail 2....%d %d  %f  \n", i, percentage, diff);
 	  fail_ctr++;
 	}
+    }
+}
+
+void test_delay(void)
+{
+  int i ;
+  int percentage;
+  double diff;
+
+  struct timeval start_time;
+  struct timeval current_time;
+
+  for (i=1;i<200;i++)
+    {
+      gettimeofday( &start_time, NULL);
+      delay (MS_DELAY1);
+      gettimeofday( &current_time, NULL);
+
+      diff = ((current_time.tv_sec  - start_time.tv_sec) * 1000000 +
+	      (current_time.tv_usec - start_time.tv_usec));
+      
+      percentage = (diff / (MS_DELAY1*1000) - 1)*100; 
+
+/*       printf ("perc: %d \n", percentage); */
+
+      if (percentage>4)
+	{
+	  fail_ctr++;
+	}
+      else
+	{
+	  succ_ctr++;
+	}
+
     }
 }
 
@@ -91,6 +135,8 @@ void test_micros(void)
 int main(void)
 {
   test_micros();
+
+  test_delay();
 
   printf ("Fails:       %d\n", fail_ctr);
   printf ("Successes:   %d\n", succ_ctr);
