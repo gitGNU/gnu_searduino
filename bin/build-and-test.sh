df6ab0384b6e8b4cd0616240c1234083b5eb40e7
Henrik Sandklef <hesa@sandklef.com>
2012-01-31 01:04:20 +0100

    Unused commands removed

--- bin/build-and-test.sh
+++ bin/build-and-test.sh
@@ -6,25 +6,7 @@ TMP_INST=/tmp/TMP_INSTALL3
 rm -fr /tmp/TMP_INSTALL3/ && \
 make -f Makefile.git      && \
 ./configure --prefix=${TMP_INST}  --enable-unittest && \
-make clean && make 
-
-exit
-
-#&& \
-cd mk/ && make clean && make && make install && cd .. && \
-cd arduino-sources/ && make clean && make && make install && cd .. && \
-cd faked-arduino/ && make clean && make && make install && cd .. && \
-cd extensions/ && make clean && make && make install && cd .. && \
-cd example/ && make clean && make && make install && cd .. && \
-echo "Test 1 in vcs dir " && \
-cd test/ && cd hw && make clean && make && make check && cd ../.. && \
-echo "Test 2 in vcs dir " && \
-cd test/ && cd shared && make clean && make && make check && cd ../.. && \
-echo "Test 3 in vcs dir " && \
-cd test/ && cd shared-main && make clean && make && make check && cd ../.. && \
-echo "Test 4 in vcs dir " && \
-cd test/ && cd stub && make clean && make && make check && cd ../..  && \
-echo "TEST 1 in dest dir" && \
+make clean && make && make check && make install && \
 pushd ${TMP_INST}/share/searduino/example/digpins/ && make -f Makefile.digpins clean all && popd && \
 echo "TEST 2 in dest dir" && \
 pushd ${TMP_INST}/share/searduino/example/python-digcounter && make -f Makefile.digcounter clean all && popd \
