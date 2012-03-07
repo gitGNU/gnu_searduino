6eb84abacc102bf9874818afb8248b1095cc02b8
Henrik Sandklef <hesa@sandklef.com>
2012-03-07 10:01:57 +0100

    minot ch

--- bin/build-and-test.sh
+++ bin/build-and-test.sh
@@ -95,48 +95,15 @@ dist()
 
 
 
-test_dist()
-{
-    DIST_FILE=$(ls -1t seard*.tar.gz | head -1)
-    echo "Working with: $DIST_FILE"
-    CUR_DIR=$(pwd)
-
-    rm -fr /tmp/TMP-SEARD
-    mkdir  /tmp/TMP-SEARD
-    cd     /tmp/TMP-SEARD
-
-    
-    tar zxvf ${CUR_DIR}/${DIST_FILE}
-    exit_on_failure $? "Unpacking DIST_FILE: ${CUR_DIR}/${DIST_FILE}"
-
-    cd *
-    exit_on_failure $? "Changing directory"
-    
-    ./configure --prefix=/tmp/TMP-SEARD-INSTALL --enable-unittest 
-    exit_on_failure $? "configure"
-
-    make
-    exit_on_failure $? "make"
-
-    make check
-    exit_on_failure $? "make check"
-
-    make install
-    exit_on_failure $? "make install"
-
-    cd /tmp/TMP-SEARD-INSTALL/share/searduino/example/digpins/ && make -f Makefile.digpins clean all
-    exit_on_failure $? "make digpins in install dir (testing dist) (in $(pwd))"
-    
-    cd /tmp/TMP-SEARD-INSTALL/share/searduino/example/python-digcounter/ && make -f Makefile.digcounter clean all
-    exit_on_failure $? "make digcounter in installed dir (testing dist)  (in $(pwd))"
-
-}
 
 prepare
 build
 doc
 check
 dist
-test_dist
 cov_prepare
 cov
+
+DIST_FILE=$(ls -1t seard*.tar.gz | head -1)
+echo "To test the dist:"
+echo "#  bin/test-dist.sh $DIST_FILE"
