3b72327bea9512b9fb5a18e410d436f314580e2a
Henrik Sandklef <hesa@sandklef.com>
2012-03-09 13:29:35 +0100

    script to launch stream simulator

---
+++ test/examples/test-c/launch-sim.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+SEARDUINO_PATH=/tmp/TMP_INSTALL3/
+ARD_CODE=../
+
+export LD_LIBRARY_PATH=${SEARDUINO_PATH}/lib
+
+${SEARDUINO_PATH}/bin/searduino-stream-sim --arduino-code ../arduino-code-dynamic/libarduino-test-code.so
+
