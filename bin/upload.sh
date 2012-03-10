8f54eb2f38045741e9978a40ac96cbfef5cf262e
Henrik Sandklef <hesa@sandklef.com>
2012-03-10 17:06:17 +0100

    first rev

---
+++ bin/upload.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+DIST_FILE=$1
+
+if [ ! -f $DIST_FILE ] || [ "$DIST_FILE" = "" ]
+then
+    echo "Dist file '$DIST_FILE' not found..."
+    exit 1
+fi
+
+echo "Signing $DIST_FILE"
+gpg -b $DIST_FILE
+
+echo "Verifying"
+gpg --verify $DIST_FILE
+
+echo "Changing permissions"
+chmod 644 ${DIST_FILE} 
+chmod 644 ${DIST_FILE}.sig
+
+copy_file()
+{
+    FILE=$1
+    DEST_BASE=dl.sv.nongnu.org:/releases/searduino
+
+    if [ "$(echo $FILE | grep bin)" != "" ]
+	then
+	DEST=$DEST_BASE/bin
+    elif [ "$(echo $FILE | grep doc)" != "" ]
+	then
+	DEST=$DEST_BASE/doc
+    else
+	DEST=$DEST_BASE/src
+    fi
+
+    echo "Copying $FILE to $DEST"
+    echo "Ok?  [y/*]"
+    read TMP_ANSWER
+    if [ "$TMP_ANSWER" != "y" ]
+    then
+	echo "Did NOT do:"
+	echo "   scp  $FILE $DEST"
+    else
+	echo "#  scp  $FILE $DEST"
+    fi
+}
+
+
+echo "Copying files"
+for i in ${DIST_FILE}  ${DIST_FILE}.sig  
+do
+    copy_file $i
+done
