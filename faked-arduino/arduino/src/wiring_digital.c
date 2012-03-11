c0376d70070a311560346408b20cc00cc2a0ee32
Henrik Sandklef <hesa@sandklef.com>
2012-03-11 12:00:48 +0100

    Adding support for limiting callbacks and keep track of last update

--- faked-arduino/arduino/src/wiring_digital.c
+++ faked-arduino/arduino/src/wiring_digital.c
@@ -40,7 +40,8 @@ typedef struct arduino_pin
 {
   uint8_t mode;
   uint8_t val;
-  struct timeval  last_time;
+  struct timeval  last_write;
+  struct timeval  last_actual_write;
 } arduino_pin;
 
 /* 
@@ -60,6 +61,21 @@ static arduino_pin arduino_digital_pins[NR_OF_DIGITAL_PINS];
 #define set_digital_pin_mode(pin,mode)  arduino_digital_pins[pin].mode=mode; 
 #define get_digital_pin_val(pin)       (arduino_digital_pins[pin].val) 
 #define set_digital_pin_val(pin,val)    arduino_digital_pins[pin].val=(val!=0);
+
+static unsigned int digitalWrite_timelimit = 500;
+
+void 
+set_digitalWrite_timelimit(unsigned int lim)
+{
+  digitalWrite_timelimit = lim;
+}
+
+unsigned int 
+get_digitalWrite_timelimit(void)
+{
+  return digitalWrite_timelimit ;
+}
+
 
 /*
  *
@@ -176,28 +192,13 @@ void digitalWrite(uint8_t pin, uint8_t val)
 
   PRINT_FUNCTION_NAME(("%d,%d",pin,val));
 
-  if( gettimeofday( &cur_time, &zoneData) == 0 )
-    {
-      if (arduino_digital_pins[pin].last_time.tv_sec!=0)
-	{
-	  time_diff = (cur_time.tv_sec - arduino_digital_pins[pin].last_time.tv_sec) * 1000000 +
-	    cur_time.tv_usec - arduino_digital_pins[pin].last_time.tv_usec ;
-
-	  /* if (time_diff<20) */
-	  /*   { */
-	  fprintf (stderr,"micro seconds since last update on pin %d : %lu\n", 
-		   pin,
-		   time_diff);
-	  /* } */
-	}
-      arduino_digital_pins[pin].last_time = cur_time;
-    }
   if (PIN_OUT_OF_RANGE(pin))
     {
       SEARD_ERROR(SEARD_ARDUINO_OUT_OF_BOUND);
       return;
     }
 
+  /*
   if ( get_digital_pin_mode(pin) != OUTPUT )
     {
       fprintf(stderr, 
@@ -206,6 +207,18 @@ void digitalWrite(uint8_t pin, uint8_t val)
       SEARD_ERROR(SEARD_ARDUINO_WRONG_PIN_MODE);
       return;
     }
+  */
+
+  if( gettimeofday( &cur_time, &zoneData) == 0 )
+    {
+      if (arduino_digital_pins[pin].last_actual_write.tv_sec!=0)
+	{
+	  time_diff = (cur_time.tv_sec - arduino_digital_pins[pin].last_actual_write.tv_sec) * 1000000 +
+	    cur_time.tv_usec - arduino_digital_pins[pin].last_actual_write.tv_usec ;
+
+	}
+    }
+  arduino_digital_pins[pin].last_write = cur_time;
 
   /*
    *
@@ -216,14 +229,33 @@ void digitalWrite(uint8_t pin, uint8_t val)
     {
       set_digital_pin_val(pin,val);
       /* printf ("wiring_digital()   (will call comm) pin:%d=%d  \n" ,pin,val);  */
-      
-      ret = comm_digital_write_outpin(pin,val);
-      if (ret != SEARD_ARDUINO_OK)
+
+      /*
+       *
+       */
+      if (time_diff<digitalWrite_timelimit) 
 	{
-	  SEARD_ERROR(ret);
-	  return;
+	  	  fprintf (stderr,"**** NO  micro seconds since last update on pin %d : %lu   (%lu)\n", 
+		   pin,
+		   time_diff, digitalWrite_timelimit);
+	  
+	} 
+      else
+	{
+	  arduino_digital_pins[pin].last_actual_write = cur_time ;     
+	  fprintf (stderr,"**** YES micro seconds since last update on pin %d : %lu  (%lu  %lu %lu)\n", 
+		   pin,
+		   time_diff,digitalWrite_timelimit, 
+		   (arduino_digital_pins[pin].last_write.tv_sec*1000000+arduino_digital_pins[pin].last_write.tv_usec),
+		   (arduino_digital_pins[pin].last_actual_write.tv_sec*1000000+arduino_digital_pins[pin].last_actual_write.tv_usec));
+	  ret = comm_digital_write_outpin(pin,val);
+	  if (ret != SEARD_ARDUINO_OK)
+	    {
+	      SEARD_ERROR(ret);
+	      return;
+	    }
 	}
-      fprintf (stderr, "  discarded calls (not on change) so far: %d\n", discard_ctr);
+      //      fprintf (stderr, "  discarded calls (not on change) so far: %d\n", discard_ctr);
     }
   else
     {
