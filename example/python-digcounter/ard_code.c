de2f8710c9d871d800ae1c698ed9d295d7014feb
Henrik Sandklef <hesa@sandklef.com>
2012-03-12 22:57:41 +0100

    removed code that is now in setup.c. Changes to test analog stuff better

--- example/python-digcounter/ard_code.c
+++ example/python-digcounter/ard_code.c
@@ -24,6 +24,7 @@
 #include <Arduino.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "setup.h"
 
 #define FIRST_LED 2
 #define LAST_LED  8
@@ -31,66 +32,52 @@
 #define ACK_PIN 5
 #define TICK_PIN 1
 
-int searduino_exec = 1 ;
-
 
 void setup(void) {
   int i ; 
 
   pinMode(1, OUTPUT);      
+  pinMode(3, OUTPUT);      
 
-  for (i=FIRST_LED;i<=LAST_LED;i++)
-    {
-      pinMode(i, OUTPUT);      
-    }
 }
 
 #define DEBUG_PIN_OUT2
-#define ENABLE_SLEEP
 
-#ifdef ENABLE_SLEEP
-#define   SEARDUINO_LOOP() for (;;) if (!searduino_exec) { fprintf (stderr, "z"); usleep(1000*200); } else 
-#else
-#define   SEARDUINO_LOOP() for (;;) 
-#endif
 
-#ifdef DEBUG_PIN_OUT
-#define   DEBUG_MSG(str)    printf("%s\n", str);fflush(stdout);
-#define   DEBUG_IN_PIN(pin) printf(" %d ", pin);fflush(stdout);
-#else
-#define   DEBUG_MSG(str)    
-#define   DEBUG_IN_PIN(pin) 
-#endif
 
 int main(void)
 {
   int i =0;
   int j ;
 
-  //  init();   
+  init();   
 
   setup();
 
   
   SEARDUINO_LOOP()
     {
-      DEBUG_MSG("input  pins (Arduino): ");
-      DEBUG_IN_PIN(digitalRead(i%2));
+      //      DEBUG_MSG("input  pins (Arduino): ");
+      //      DEBUG_IN_PIN(digitalRead(i%2));
       digitalWrite(1,i%2);
-      
+      digitalWrite(3,digitalRead(2));
+
+      analogWrite(1,i);
+      analogWrite(3,analogRead(2));
+
+
+      /*      
       for (j=FIRST_LED;j<=LAST_LED;j++)
 	{
-	  //	  DEBUG_IN_PIN(digitalRead(j));
-	  //	  digitalWrite(j,digitalRead(j));
 	  digitalWrite(j,(i+j)%2);
 	  usleep(1000*100); 
 	}
       DEBUG_MSG("");
-      
+      */
       i++;
       if (i==8) i=0;
 
-      usleep(1000*1000*2); 
+      usleep(1000*500); 
     }
   DEBUG_MSG("");
 }
