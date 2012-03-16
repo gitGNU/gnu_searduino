082743f2a6e704634c8d6f51030285d0ebfa7ee4
Henrik Sandklef <hesa@sandklef.com>
2012-03-16 22:31:44 +0100

    merge

--- example/python-digcounter/ard_code.c
+++ example/python-digcounter/ard_code.c
@@ -26,13 +26,6 @@
 #include <unistd.h>
 #include "setup.h"
 
-#define FIRST_LED 2
-#define LAST_LED  8
-
-#define ACK_PIN 5
-#define TICK_PIN 1
-
-
 void setup(void) {
   int i ; 
 
@@ -41,14 +34,10 @@ void setup(void) {
 
 }
 
-#define DEBUG_PIN_OUT2
-
-
 
 int main(void)
 {
-  int i =0;
-  int j ;
+  int i=0;
 
   init();   
 
@@ -57,28 +46,14 @@ int main(void)
   
   SEARDUINO_LOOP()
     {
-      //      DEBUG_MSG("input  pins (Arduino): ");
-      //      DEBUG_IN_PIN(digitalRead(i%2));
       digitalWrite(1,i%2);
       digitalWrite(3,digitalRead(2));
 
       analogWrite(1,i);
       analogWrite(3,analogRead(2));
 
-
-      /*      
-      for (j=FIRST_LED;j<=LAST_LED;j++)
-	{
-	  digitalWrite(j,(i+j)%2);
-	  usleep(1000*100); 
-	}
-      DEBUG_MSG("");
-      */
       i++;
-      if (i==8) i=0;
-
-      usleep(1000*500); 
+      usleep(1000*200); 
     }
-  DEBUG_MSG("");
 }
 
