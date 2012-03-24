7b5149645261421385001cce476b0e971758bbe3
Henrik Sandklef <hesa@sandklef.com>
2012-03-24 11:03:11 +0100

    New dir layout. Added I path

--- mk/searduino-stub.mk
+++ mk/searduino-stub.mk
@@ -14,6 +14,7 @@ JAVAC=gcj
 
 INC_FLAGS= -I$(SEARDUINO_INC_PATH)/arduino-sources/core \
            -I$(SEARDUINO_INC_PATH)/arduino/             \
+           -I$(SEARDUINO_INC_PATH)/arduino-extras/include   \
            -I$(SEARDUINO_INC_PATH)/                     \
            -I$(SEARDUINO_INC_PATH)/arduino-sources/variants/standard  \
 #           -I$(SEARDUINO_INC_PATH)/boards  \
