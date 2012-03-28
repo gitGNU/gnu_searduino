dac2a1d6193d376d08b018e973b41594ad144b7b
Henrik Sandklef <hesa@sandklef.com>
2012-03-28 22:14:18 +0200

    first rev

---
+++ test/arduino-lib/functions.h
@@ -0,0 +1,31 @@
+/*****
+ *                                                                   
+ *                   Searduino
+ *                      
+ *   Copyright (C) 2012 Henrik Sandklef 
+ *                                                                   
+ * This program is free software; you can redistribute it and/or     
+ * modify it under the terms of the GNU General Public License       
+ * as published by the Free Software Foundation; either version 3    
+ * of the License, or any later version.                             
+ *                                                                   
+ *                                                                   
+ * This program is distributed in the hope that it will be useful,   
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of    
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     
+ * GNU General Public License for more details.                      
+ *                                                                   
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software       
+ * Foundation, Inc., 51 Franklin Street, Boston,            
+ * MA  02110-1301, USA.                                              
+ ****/
+
+#include <Arduino.h>
+#include "searduino.h"
+
+void
+functions_set_digital_pin_mode(uint8_t pin, uint8_t mode);
+
+void
+functions_set_digital_pin(uint8_t pin, uint8_t val);
