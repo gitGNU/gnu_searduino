3318f9bf9767a44e303c8b5e5a2584d701744cd0
Henrik Sandklef <hesa@sandklef.com>
2012-04-15 01:19:23 +0200

    examples on serial

---
+++ example/serial/main.c
@@ -0,0 +1,46 @@
+/*****
+ *                                                                   
+ *                   Searduino
+ *                      
+ *   Copyright (C) 2011, 2012 Henrik Sandklef 
+ *                                                                   
+ * This program is free software; you can redistribute it and/or     
+ * modify it under the terms of the GNU General Public License       
+ * as published by the Free Software Foundation; either version 3    
+ * of the License, or any later version.                             
+ *                                                                   
+ *                                                                   
+ * This program is distributed in the hope that it will be useful,   
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of    
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     
+ * GNU General Public License for more details.                      
+ *                                                                   
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software       
+ * Foundation, Inc., 51 Franklin Street, Boston,            
+ * MA  02110-1301, USA.                                              
+ ****/
+
+#include <Arduino.h>
+#include "searduino.h"
+
+void setup() {
+  pinMode(13, OUTPUT);      
+}
+
+
+int main(void)
+{
+  uint8_t i ;
+  
+  init();  
+  setup();
+  
+  SEARDUINO_LOOP()
+    {
+      i++;
+      serial_stuff();
+      delay(10);
+    }
+  return 0;
+}
