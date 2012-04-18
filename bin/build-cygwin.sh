a2126362b65a9ad83ac3bbd30f4d121d42fada97
Henrik Sandklef <hesa@sandklef.com>
2012-04-18 22:16:53 +0200

    Yet another typo fixed

--- bin/build-cygwin.sh
+++ bin/build-cygwin.sh
@@ -5,6 +5,6 @@ cat configure.ac.orig | \
   sed -e 's,AM_MISSING_PROG,#AM_MISSING_PROG,g' \
       -e 's,AC_PYTHON_DEVEL,#AC_PYTHON_DEVEL,g' > configure.ac 
 
-make -f Makfile.git && \
+make -f Makefile.git && \
   ./configure --disable-python --disable-unittest && \
   make && make install
