114d7ed226271b06cb5ad88ec44f078e198368c1
Henrik Sandklef <hesa@sandklef.com>
2012-04-22 10:49:26 +0200

    Trying to build for win on GNU... not yet working

--- bin/build-win.sh
+++ bin/build-win.sh
@@ -15,17 +15,29 @@ fi
 init_logging
 
 
-CONF_FLAGS="--host i686-w64-mingw32"
+CONF_FLAGS="--host=i686-w64-mingw32"
 CONF_FLAGS="$CONF_FLAGS  --disable-python"
 CONF_FLAGS="$CONF_FLAGS  --prefix=/opt/searduino-win"
 CONF_FLAGS="$CONF_FLAGS  --disable-unittest"
-PRE_FLAGS="CFLAGS=\"-I/opt/searduino-win/dlfcn/include/\""
-PRE_FLAGS="$PRE_FLAGS  LDFLAGS=\"-I/opt/searduino-win/dlfcn/lib -ldl\""
 
 export SEARD_OS=CYGWIN
-#log_and_exec   make -f Makefile.git
-#log_and_exec   $PRE_FLAGS ./configure $CONF_FLAGS
-echo   $PRE_FLAGS ./configure $CONF_FLAGS
+log_and_exec   make -f Makefile.git
+
+LOCAL_START_SEC=$(date '+%s')
+logn "$*"
+export CFLAGS="-I/opt/searduino-win/dlfcn/include/ " 
+#export CFLAGS="-I/opt/searduino-win/dlfcn/include/ -I/opt/searduino-win/pthread/include/" 
+#export CFLAGS="-I/home/hesa/cyg-dev/include " 
+#export LDFLAGS="-I/home/hesa/cyg-dev/lib " 
+export LDFLAGS="-L/opt/searduino-win/dlfcn/lib -ldl"
+# -L/opt/searduino-win/pthread/lib -lpthread"
+./configure $CONF_FLAGS   >> $REAL_LOG_FILE  2>> $REAL_LOG_FILE 
+exit_on_failure $? ""
+
+#rm -fr /opt/searduino/*
+#log_and_exec   make
+#log_and_exec   make install
+
 
 
 
