a804b1fa18a6a627122fb0e43f04324cbe9923e9
Henrik Sandklef <hesa@sandklef.com>
2012-04-24 23:50:30 +0200

    Adding dummy impl of pulseIn

---
+++ faked-arduino/arduino/src/wiring_pulse.c
@@ -0,0 +1,31 @@
+/*****
+ *                                                                   
+ *                   Searduino
+ *                      
+ *   Copyright (C) 2012 Henrik Sandklef 
+ *                                                                   
+ * This program is free software; you can redistribute it and/or     
+ * modify it under the terms of the GNU General Public License       
+ * as published by the Free Software Foundation; either version 3    
+ * of the License, or any later version.                             
+ *                                                                   
+ *                                                                   
+ * This program is distributed in the hope that it will be useful,   
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of    
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     
+ * GNU General Public License for more details.                      
+ *                                                                   
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software       
+ * Foundation, Inc., 51 Franklin Street, Boston,            
+ * MA  02110-1301, USA.                                              
+ ****/
+
+#include "wiring_private.h"
+#include "pins_arduino.h"
+
+unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
+{
+  PRINT_DUMMY_FUNCTION_IMPLEMENTATION();
+  return 100; 
+}
