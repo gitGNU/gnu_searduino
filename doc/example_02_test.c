435145e96a6bc71d18ba98808633cc960c23037d
Henrik Sandklef <hesa@sandklef.com>
2012-04-24 16:58:59 +0200

    Minor change to make texinfo generation work

---
+++ doc/example_02_test.c
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <searduino.h>
+#include <seasim.h>
+
+
+void setup(void)
+{
+  pinMode(13, OUTPUT);
+}
+
+
+
+int main(void)
+{
+  uint8_t i = 0;
+  uint8_t j = 0 ;
+  int ctr=0;
+  init();
+
+  setup();
+
+  for(i=0;;i++)
+    {    
+      /* Set the analog pin 11 & 12, 
+	 which is used by get_distance() */
+      seasim_set_generic_input(11, i, INPUT);
+      seasim_set_generic_input(12, j, INPUT);
+      
+      /*
+	printf ("get_distance()=%.4d  (i:%.4d  j:%.4d  ctr:%.4d)\n", 
+	get_distance(), i, j , ctr++);
+      */
+      
+      if (get_distance()!=(uint8_t)(i+j))
+	{
+	  printf(" ERROR: %d != %d\n", get_distance(), (uint8_t)(i+j));
+	  return 1;
+	}
+      if (i==255) 
+	{
+	  j++;    
+	}
+      if (j==255) 
+	{ 
+	  break ; 
+	}
+    }  
+  return 0;
+}
