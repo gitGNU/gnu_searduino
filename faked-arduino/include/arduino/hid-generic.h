43ca217a559b908639e528320e63f7f2b4438b20
Henrik Sandklef <hesa@sandklef.com>
2012-07-03 22:42:10 +0200

    ifdef/define/endif closure added

--- faked-arduino/include/arduino/hid-generic.h
+++ faked-arduino/include/arduino/hid-generic.h
@@ -1,8 +1,35 @@
+/*****
+ *                                                                   
+ *                   Searduino
+ *                      
+ *   Copyright (C) 2012 Henrik Sandklef 
+ *                                                                   
+ * This program is free software; you can redistribute it and/or     
+ * modify it under the terms of the GNU General Public License       
+ * as published by the Free Software Foundation; either version 3    
+ * of the License, or any later version.                             
+ *                                                                   
+ *                                                                   
+ * This program is distributed in the hope that it will be useful,   
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of    
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     
+ * GNU General Public License for more details.                      
+ *                                                                   
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software       
+ * Foundation, Inc., 51 Franklin Street, Boston,            
+ * MA  02110-1301, USA.                                              
+ ****/
+
+
 /* TEMPORARILY WHILE DEVELOPING */
 #define  USE_X11
 /* EO TEMP */
 
 #include "hid-x11.h"
+
+#ifndef HID_GENERIC_H
+#define HID_GENERIC_H
 
 enum KEY_STATES
   {
@@ -34,3 +61,4 @@ int hid_initialise_hid(void);
 int hid_enable_faked_hid(void);
 int hid_disable_faked_hid(void);
 
+#endif /* HID_GENERIC_H */
