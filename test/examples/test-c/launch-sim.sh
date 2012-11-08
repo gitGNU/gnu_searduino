6032778232299bd92076c4c2f31174d022b97ac4
Henrik Sandklef <hesa@sandklef.com>
2012-11-08 23:25:11 +0100

    MacOS fix to load dyn libs

--- test/examples/test-c/launch-sim.sh
+++ test/examples/test-c/launch-sim.sh
@@ -5,6 +5,7 @@ SEARDUINO_PATH=/tmp/TMP_INSTALL3/
 ARD_CODE=../
 
 export LD_LIBRARY_PATH=${SEARDUINO_PATH}/lib
+export DYLD_LIBRARY_PATH=${SEARDUINO_PATH}/lib
 
 ${SEARDUINO_PATH}/bin/searduino-stream-sim --arduino-code ../arduino-code-dynamic/libarduino-test-code.so
 
