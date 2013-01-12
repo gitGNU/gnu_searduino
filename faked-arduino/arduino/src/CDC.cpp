44f0b9f0b18a48c89f737ff4115c433931102a12
Henrik Sandklef <hesa@sandklef.com>
2013-01-12 11:33:15 +0100

    Typo fixed

--- faked-arduino/arduino/src/CDC.cpp
+++ faked-arduino/arduino/src/CDC.cpp
@@ -8,7 +8,7 @@
 #if defined(USBCON)
 #ifdef CDC_ENABLED
 
-static int cdc_searial_initialised = 0;
+static int cdc_serial_initialised = 0;
 
 
 
@@ -30,7 +30,7 @@ bool CDC_Setup(Setup& setup)
 int _serialPeek = -1;
 void Serial_::begin(uint16_t baud_count)
 {
-  cdc_searial_initialised = 1;
+  cdc_serial_initialised = 1;
 }
 
 void Serial_::end(void)
@@ -66,7 +66,7 @@ void Serial_::flush(void)
 
 size_t Serial_::write(uint8_t c)
 {
-  if ( cdc_searial_initialised )
+  if ( cdc_serial_initialised )
     {
       Serial.println(c);
     }
