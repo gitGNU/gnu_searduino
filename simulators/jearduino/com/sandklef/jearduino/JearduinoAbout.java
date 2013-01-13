a560bf8a7bcfb932dcafe955f8abd9f55d5b2583
Henrik Sandklef <hesa@sandklef.com>
2013-01-13 14:31:27 +0100

    Adding (c) and license info

--- simulators/jearduino/com/sandklef/jearduino/JearduinoAbout.java
+++ simulators/jearduino/com/sandklef/jearduino/JearduinoAbout.java
@@ -58,7 +58,7 @@ public class JearduinoAbout extends JDialog {
 
         JLabel filler = new JLabel("Searduino was written by:");
         filler.setHorizontalAlignment(JLabel.CENTER);
-        panel.setLayout(new GridLayout(1, 2));
+        panel.setLayout(new GridLayout(2, 1));
         panel.add(filler);
 
 	JTextArea textArea = new JTextArea(5, 20);
@@ -71,6 +71,7 @@ public class JearduinoAbout extends JDialog {
         return panel;
     }
 
+
     public JearduinoAbout()
     {        
 
@@ -78,9 +79,11 @@ public class JearduinoAbout extends JDialog {
 
         JComponent aboutPanel = makeAboutPanel();
         JComponent authorPanel = makeAuthortPanel();
+        JComponent licensePanel = makeLicensePanel();
 
 	tabbedPane.addTab("About", aboutPanel);
 	tabbedPane.addTab("Authors", authorPanel);
+	tabbedPane.addTab("Copyright and License", licensePanel);
 
 	//	add(tabbedPane);
 
@@ -102,4 +105,37 @@ public class JearduinoAbout extends JDialog {
 
 	setMinimumSize(new Dimension(400, 400));
     }
+
+
+    protected JComponent makeLicensePanel() {
+        JPanel panel = new JPanel(false);
+
+	JTextArea infoArea = new JTextArea();
+	JTextArea licenseArea = new JTextArea();
+
+        panel.setLayout(new GridLayout(2, 1));
+        panel.add(infoArea);
+        panel.add(licenseArea);
+
+	JScrollPane scrollPane = new JScrollPane(licenseArea); 
+	licenseArea.setEditable(false);
+	infoArea.setEditable(false);
+        panel.add(infoArea);
+        panel.add(licenseArea);
+
+        infoArea.append("Searduino is currently copyrighted to the authors\n");
+        infoArea.append(" * see individual files for more information\n");
+	infoArea.append("Searduino is Free Software - software that gives you 4 freedoms:\n");
+	infoArea.append(" * The freedom to run the program, for any purpose (freedom 0).\n");
+	infoArea.append(" * The freedom to study how the program works, and change it so it does your computing as you wish (freedom 1).\n");
+	infoArea.append(" * The freedom to redistribute copies so you can help your neighbor (freedom 2).\n");
+	infoArea.append(" * The freedom to distribute copies of your modified versions to others (freedom 3).\n");
+	infoArea.append("\nRead more about free software at http://www.gnu.org/philosophy/free-sw.html\n\n");
+	
+	licenseArea.append("Searduino is licensed under GPLv3\n");
+	licenseArea.append(" See http://www.gnu.org/licenses/gpl-3.0.txt for more iformation\n");
+	
+        return panel;
+    }
+
 }
