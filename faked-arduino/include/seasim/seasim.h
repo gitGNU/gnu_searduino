5118bc946f6b7687af636ef2b2a8db1dde60520b
Henrik Sandklef <hesa@sandklef.com>
2013-01-15 23:22:49 +0100

    Added comments to functions

--- faked-arduino/include/seasim/seasim.h
+++ faked-arduino/include/seasim/seasim.h
@@ -100,82 +100,379 @@ seasim_fake_analog_input (uint8_t pin, unsigned int val);
 
 
   /*
-   * This functions will be obsoleted.
+   * This functions is obsoleted.
    * It is replaced by the easier to use: 
    *   seasim_fake_digital_input (uint8_t pin, uint8_t val);
    *   seasim_fake_analog_input (uint8_t pin, unsigned int val);
-   */
 uint8_t 
 seasim_set_generic_input(uint8_t pin, unsigned int val, uint8_t pin_type);
+   */
 
   /*
-   * This functions will be obsoleted.
+   * This functions is obsoleted.
    * It is replaced by the easier to use: 
-   */
 unsigned int
 seasim_get_generic_output(uint8_t pin, uint8_t pin_type);
+   */
 
+/**
+ * @name    seasim_fake_input
+ * @brief   Fakes input on a general Arduino pin
+ *
+ * This function should be used if you want to simulate input on the
+ * Arduino boards. The coresponds to actually setting the input
+ * manually (e.g using power supply) an input pin on the Arduino.
+ *
+ * @param [uint8_t] pin Analog or Digital pin to fake input on
+ * @param [uint8_t] value Value (0 - 1024) to fake
+ * @param [uint8_t] type type of pin (SEARDUINO_PIN_TYPE_DIGITAL, SEARDUINO_PIN_TYPE_ANALOG)
+ *
+ * @retval 0 on success
+ * @retval !0 on failure
+ *
+ * Example Usage:
+ * @code
+ *    seasim_fake_input (8, 786, SEARDUINO_PIN_TYPE_ANALOG);
+ *    seasim_fake_input (13,1,   SEARDUINO_PIN_TYPE_DIGITAL);
+ * @endcode
+ */
 uint8_t
-seasim_set_input(uint8_t pin, unsigned int val, uint8_t pin_type);
+seasim_fake_input(uint8_t pin, unsigned int val, uint8_t type);
 
+/**
+ * @name    seasim_get_output
+ * @brief   Returns output from a general Arduino pin
+ *
+ * This function should be used if you want to check the current output
+ * on the an Arduino board pin. The output is set by the application running 
+ * in the simulated environment (the code you'll put on the Arduino board). 
+ *
+ * @param [uint8_t] pin Analog or Digital pin to fake input on
+ * @param [uint8_t] type type of pin (SEARDUINO_PIN_TYPE_DIGITAL, SEARDUINO_PIN_TYPE_ANALOG)
+ *
+ * @retval [int] the value of the pin
+ *
+ * Example Usage:
+ * @code
+ *    seasim_fake_input (13, SEARDUINO_PIN_TYPE_ANALOG);
+ * @endcode
+ */
 int
 seasim_get_output(uint8_t pin, uint8_t pin_type);
 
+/**
+ * @name    seasim_is_pausable
+ * @brief   Check if you can pause the Arduino code
+ *
+ * Searduino provides some macros allowing the simluator to pause
+ * the code. 
+ *
+ * @retval [uint8_t] 0 if you can NOT pause the application. Non zero if you can.
+ *
+ */
 uint8_t 
 seasim_is_pausable(void);
 
+/**
+ * @name    seasim_is_paused
+ * @brief   Check if you Arduino code is paused
+ *
+ * Searduino provides some macros allowing the simluator to pause
+ * the code. 
+ *
+ * @retval [uint8_t] 0 if the application is paused. Non zero if it is not.
+ *
+ */
 uint8_t 
 seasim_is_paused(void);
 
+/**
+ * @name    seasim_is_paused
+ * @brief   Check if you Arduino code is halted (stopped)
+ *
+ *
+ * @retval [uint8_t] 0 if the application is halted (stopped). Non zero if it is still running.
+ *
+ */
 uint8_t 
 seasim_is_halted(void);
 
+/**
+ * @name    seasim_is_running
+ * @brief   Check if you Arduino code is running
+ *
+ *
+ * @retval [uint8_t] 0 if the application is running. Non zero if it is not running.
+ *
+ */
 uint8_t 
 seasim_is_running(void);
 
+/**
+ * @name    seasim_set_paused
+ * @brief   Pauses the Arduino code 
+ *
+ * Searduino provides some macros allowing the simluator to pause
+ * the code. If you're not using these this function has no impact on the 
+ * running Arduino code.
+ *
+ * @retval [void]
+ *
+ */
 void 
 seasim_set_paused(void);
 
+/**
+ * @name    seasim_set_running
+ * @brief   Starts the Arduino code 
+ *
+ * Searduino provides some macros allowing the simluator to pause
+ * the code. If you're not using these this function has no impact on the 
+ * running Arduino code.
+ *
+ * @retval [void]
+ *
+ */
 void 
 seasim_set_running(void);
 
+
+/**
+ * @name    seasim_set_halted
+ * @brief   Halts the Arduino code 
+ *
+ * Searduino provides some macros allowing the simluator to pause
+ * the code. If you're not using these this function has no impact on the 
+ * running Arduino code.
+ *
+ * @retval [void]
+ *
+ */
 void 
 seasim_set_halted(void);
 
+/**
+ * @name    seasim_get_dig_mode
+ * @brief   Returns the mode from a digital Arduino pin
+ *
+ * @param [uint8_t] pin Analog or Digital pin to fake input on
+ *
+ * @retval [int] the mode
+ *
+ * Example Usage:
+ * @code
+ *    seasim_get_dig_mode (13);
+ * @endcode
+ */
 uint8_t 
 seasim_get_dig_mode(uint8_t pin);
 
+
+/**
+ * @name    seasim_get_arduino_code_name
+ * @brief   Returns the name of the Arduino code (file name) loaded
+ *
+ * The returned string must not be freed. If you need to modify it
+ * you must copy the string.
+ * 
+ * @retval [char*] the name of the Arduino code running
+ *
+ * Example Usage:
+ * @code
+ *    char *arduino_code;
+ *    arduino_code = seasim_get_arduino_code_name();
+ * @endcode
+ */
 char *
 seasim_get_arduino_code_name(void);
 
+/**
+ * @name    seasim_set_arduino_code_name
+ * @brief   Sets the name of the Arduino code (file name) loaded
+ *
+ * @param [const char*] libname The file name of the shared library (including path) you wich to load for later execution.
+ *
+ * @retval [char*] the name of the Arduino code running
+ *
+ * Example Usage:
+ * @code
+ *   int ret;
+ *   ret  = seasim_set_arduino_code_name("./my-ard-code.so");
+ * @endcode
+ */
 int 
 seasim_set_arduino_code_name(const char* libname);
 
+/**
+ * @name    seasim_register_dig_mode_sim_cb
+ * @brief   Register a callback for changes in digital modes of pin
+ *
+ * Whenever a pin changes mode on the Arduino you'll get noticed via 
+ * your registered callback.
+ *
+ * @param [dm_to_sim_callback_ptr] cb The callback
+ *
+ * @retval [uint8_t] 0 on success. Non zero otherwise.
+ *
+ * Example Usage:
+ * @code
+ *  void 
+ *  my_dm_sim_callback(uint8_t pin, uint8_t mode)
+ *  {
+ *    fprintf (stdout,"Pin %d changed mode to %d\n",pin, mode);
+ *
+ *  }
+ *  .....
+ *  int ret;
+ *  ret  = seasim_register_dig_mode_sim_cb(my_dm_sim_callback);
+ * @endcode
+ */
 uint8_t
 seasim_register_dig_mode_sim_cb(dm_to_sim_callback_ptr cb);
 
+/**
+ * @name    seasim_register_out_sim_cb
+ * @brief   Register a callback for updates on the pin outputs
+ *
+ * Whenever an output pin changes value on the Arduino you'll get noticed via 
+ * your registered callback.
+ *
+ * @param [out_to_sim_callback_ptr] cb The callback
+ *
+ * @retval [uint8_t] 0 on success. Non zero otherwise.
+ *
+ * Example Usage:
+ * @code
+ *  void
+ *  my_out_sim_callback(uint8_t pin, unsigned int value, uint8_t pin_type)
+ *  {
+ *      fprintf (stdout,
+ *	   "Pin%d value:%d type:%d\n",
+ *	   pin, 
+ *	   value, 
+ *	   pin_type);
+ *  }
+ *  .....
+ *  int ret;
+ *  ret  = seasim_register_out_sim_cb(my_out_sim_callback);
+ * @endcode
+ */
 uint8_t 
 seasim_register_out_sim_cb(out_to_sim_callback_ptr cb);
 
-uint8_t
-seasim_register_dig_mode_sim_cb(dm_to_sim_callback_ptr cb);
 
+/**
+ * @name    seasim_register_log_cb
+ * @brief   Register a callback for log written by the Arduino code
+ *
+ * Searduino provides some logging functions to the Arduino code.
+ * You can register and get those logs via a callback using this function.
+ *
+ * @param [seasim_register_log_cb] cb The callback
+ *
+ * @retval [uint8_t] 0 on success. Non zero otherwise.
+ *
+ * Example Usage:
+ * @code
+ * void
+ * my_log_sim_callback(uint8_t level, const char *str)
+ * {
+ *    fprintf (stdout,
+ *    	   "LOG CALLBACK :%d  msg:%s  %d\n",
+ *    	   level, 
+ *    	    str, strlen(str));
+ * }
+ * ....
+ * int ret;
+ * 	  ret  = seasim_register_log_cb(my_log_sim_callback);
+ * @endcode
+ */
 uint8_t
 seasim_register_log_cb(log_to_sim_callback_ptr cb);
 
+/**
+ * @name    seasim_register_type_cb
+ * @brief   Register a callback for type changes on a pin
+ *
+ * Whenever a pin changes type (e.g from digital to analog) you'll get 
+ * notified through the registered callback.
+ *
+ * @param [pintype_to_sim_callback_ptr] cb The callback
+ *
+ * @retval [uint8_t] 0 on success. Non zero otherwise.
+ *
+ * Example Usage:
+ * @code
+ * void
+ * my_type_sim_callback(uint8_t pin, uint8_t pin_type)
+ * {
+ *    fprintf (stdout,
+ *            "TYPE CALLBACK :%d  type:%d\n",
+ * 	   pin, 
+ * 	   pin_type);
+ * 
+ * }
+ * ...
+ * int ret;
+ * ret  = seasim_register_type_cb(my_type_sim_callback);
+ * 
+ * @endcode
+ */
 uint8_t
 seasim_register_type_cb(pintype_to_sim_callback_ptr cb);
 
+/**
+ * @name    seasim_register_type_cb
+ * @brief   Register a callback for type changes on a pin
+ *
+ * Setup all functionality of the Searduino simluator part.
+ * You can call this function many times - the setup is only 
+ * done once.
+ *
+ * @retval [uint8_t] 0 on success. Non zero otherwise.
+ *
+ */
 int 
 seasim_setup(void);
 
+
+/**
+ * @name    seasim_disable_streamed_output
+ * @brief   Turn off printouts on terminal
+ *
+ * Apart from using callbacks to notify simluators/tests
+ * of updates Searduino also prints to stdout to inform of changes.
+ * This function disables those printouts.
+ *
+ * The printout is enabled by default.
+ */
 void 
 seasim_disable_streamed_output(void);
 
 
+/**
+ * @name    seasim_is_enable_streamed_output
+ * @brief   Chekcs if streamed output is enabled or disabled
+ *
+ * Apart from using callbacks to notify simluators/tests
+ * of updates Searduino also prints to stdout to inform of changes.
+ * This function checks if such printout is enabled or disabled.
+ *
+ * @retval [int] 1 if enabled. 0 if not.
+ */
 uint8_t 
 seasim_is_enable_streamed_output(void);
 
+/**
+ * @name    seasim_ensable_streamed_output
+ * @brief   Turn on printouts on terminal
+ *
+ * Apart from using callbacks to notify simluators/tests
+ * of updates Searduino also prints to stdout to inform of changes.
+ * This function enables those printouts.
+ *
+ * The printout is enabled by default.
+ */
 void 
 seasim_enable_streamed_output(void);
 
