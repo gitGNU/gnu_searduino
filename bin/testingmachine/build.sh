a4cd4754ed1db2a0e43b638d739443a2be8e569a
Henrik Sandklef <hesa@sandklef.com>
2013-12-27 01:15:30 +0100

    First rev Trying out auto builds together with testingmachine

---
+++ bin/testingmachine/build.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+OS=$(uname -o)
+SCRIPT_DIR=$(dirname $0)
+
+setup_debian_ubuntu()
+{
+    DEFAULT_JAVA_PATH=/usr/lib/jvm/java-7-openjdk-amd64/include/
+    export CFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
+    export CXXFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
+
+
+    CONF_ARGS="--disable-python-extension --enable-debian-sources"
+}
+
+setup_per_dist() 
+{
+    set_dist
+    if [ "$DIST" = "Ubuntu" ]
+    then
+	setup_debian_ubuntu
+    elif [ "$DIST" = "Debian" ]
+    then
+	setup_debian_ubuntu
+    else
+	echo "No setup possible for: $DIST"
+	return 1
+    fi
+}
+
+
+
+setup_per_dist
+cd $SCRIPT_DIR
+#
+#
+#
+make -f Makefile.git 
+exit_on_error "$?" "Creating configure scripts"
+
+./configure $CONF_ARGS
+exit_on_error "$?" "Configuring"
+
+make
+exit_on_error "$?" "make"
+
+make check
+exit_on_error "$?" "make check"
+
