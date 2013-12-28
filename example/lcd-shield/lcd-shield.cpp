50e6be8cb56c47f5c4ea18edd6aeba2766ce4bee
Henrik Sandklef <hesa@sandklef.com>
2013-12-28 12:28:22 +0100

    (C) notice, misc small changes

--- example/lcd-shield/lcd-shield.cpp
+++ example/lcd-shield/lcd-shield.cpp
@@ -1,3 +1,26 @@
+/*****
+ *                                                                   
+ *                   Searduino
+ *                      
+ *   Copyright (C) 2013 Henrik Sandklef 
+ *                                                                   
+ * This program is free software; you can redistribute it and/or     
+ * modify it under the terms of the GNU General Public License       
+ * as published by the Free Software Foundation; either version 3    
+ * of the License, or any later version.                             
+ *                                                                   
+ *                                                                   
+ * This program is distributed in the hope that it will be useful,   
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of    
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     
+ * GNU General Public License for more details.                      
+ *                                                                   
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software       
+ * Foundation, Inc., 51 Franklin Street, Boston,            
+ * MA  02110-1301, USA.                                              
+ ****/
+
 #include <Arduino.h>
 #include "searduino.h"
 #include "LiquidCrystal.h"
@@ -26,7 +49,7 @@ void read_and_write() {
   x = 25;
   
 
-  Serial.print("Test");
+  Serial.println("Test");
 
   if (x < 50) {                        
     lcd.setCursor(0,0);
