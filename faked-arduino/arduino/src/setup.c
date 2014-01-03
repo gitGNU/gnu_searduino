adafd08bf86c3d6de9e2749959427318354a9f90
Henrik Sandklef <hesa@sandklef.com>
2014-01-03 19:47:54 +0100

    Bump year in (c)

--- faked-arduino/arduino/src/setup.c
+++ faked-arduino/arduino/src/setup.c
@@ -2,7 +2,7 @@
  *                                                                   
  *                   Searduino
  *                      
- *   Copyright (C) 2011, 2012, 2013 Henrik Sandklef 
+ *   Copyright (C) 2011-2014 Henrik Sandklef 
  *                                                                   
  * This program is free software; you can redistribute it and/or     
  * modify it under the terms of the GNU General Public License       
--- faked-arduino/seasim/src/seasim.c
+++ faked-arduino/seasim/src/seasim.c
@@ -2,7 +2,7 @@
  *                                                                   
  *                   Searduino
  *                      
- *   Copyright (C) 2012 Henrik Sandklef 
+ *   Copyright (C) 2012-2014 Henrik Sandklef 
  *                                                                   
  * This program is free software; you can redistribute it and/or     
  * modify it under the terms of the GNU General Public License       
