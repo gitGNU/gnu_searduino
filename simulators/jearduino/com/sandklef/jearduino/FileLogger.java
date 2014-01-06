9e3d388cee83dda1bd7fe8db37e1709eef4ca8e9
Henrik Sandklef <hesa@sandklef.com>
2014-01-06 22:21:53 +0100

    Scrollbar, clear button added. Possibility to show current settings

---
+++ simulators/jearduino/com/sandklef/jearduino/FileLogger.java
@@ -0,0 +1,107 @@
+/*****
+ *                                                                   
+ *                   Searduino
+ *                      
+ *   Copyright (C) 2014 Henrik Sandklef 
+ *                                                                   
+ * This program is free software; you can redistribute it and/or     
+ * modify it under the terms of the GNU General Public License       
+ * as published by the Free Software Foundation; either version 3    
+ * of the License, or any later version.                             
+ *                                                                   
+ *                                                                   
+ * This program is distributed in the hope that it will be useful,   
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of    
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     
+ * GNU General Public License for more details.                      
+ *                                                                   
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software       
+ * Foundation, Inc., 51 Franklin Street, Boston,            
+ * MA  02110-1301, USA.                                              
+ ****/
+
+package com.sandklef.jearduino;
+
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+
+class FileLogger extends JPanel implements TailerEvent, ActionListener {
+
+    private JTextArea textArea ;
+    Tailer tailer;
+    JButton clear;
+    JButton printState;
+    Jearduino jearduino;
+    JPanel pane;
+    JScrollPane scrollPane;
+
+    public FileLogger(String str, Jearduino j) {
+	super(new GridLayout(2, 1));    
+	setBorder(BorderFactory.createTitledBorder(str));
+
+	textArea = new JTextArea();
+	scrollPane = new JScrollPane(textArea); 
+	//	textArea.setEditable(false);
+
+	/* Button panel */
+	pane = new JPanel();
+	clear = new JButton("Clear") ;
+	printState = new JButton("Print State");
+
+	jearduino = j;
+
+	add(scrollPane);
+	add(pane);
+
+	pane.add(clear);
+	pane.add(printState);
+
+	scrollPane.setPreferredSize(new Dimension(600, 700)); 
+	//textArea.setMinimumSize (new Dimension(300, 750)); 
+
+	clear.addActionListener(this);
+	printState.addActionListener(this);
+
+
+	setVisible(true);
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+	int type = 0 ; 
+
+	if (e.getSource() == clear) {
+	    textArea.setText("");
+	} else if (e.getSource() == printState) {
+	    addLog(jearduino.getJearduinoState());
+	    addLog(jearduino.getSearduinoState());
+	}
+    }
+
+    public void handleFileInput(String text) {
+	addLog(text + "\n");
+    }
+    
+    public void start() {
+	tailer = new Tailer("/tmp/searduino-internal.log", this);
+	Thread t = new Thread(tailer);
+	t.start();
+    }
+
+    public void addLog(String data) {
+	textArea.append(data);
+	textArea.setCaretPosition(textArea.getDocument().getLength()); 
+	this.validate();
+	this.revalidate();
+	this.repaint();
+    }
+
+}
