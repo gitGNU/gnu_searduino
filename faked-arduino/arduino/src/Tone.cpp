9dc57a6413b8d918f73b3332364df99b45291511
Henrik Sandklef <hesa@sandklef.com>
2014-03-20 17:32:57 +0100

    First rev, Tone lib

---
+++ faked-arduino/arduino/src/Tone.cpp
@@ -0,0 +1,33 @@
+/* 
+ */
+
+#include "Arduino.h"
+#include "searduino.h"
+
+
+static int8_t toneBegin(uint8_t _pin)
+{
+  return 0;
+}
+
+
+
+
+void tone(uint8_t _pin, unsigned int frequency, unsigned long duration)
+{
+  printf ("TONE OTNE:...\n");
+  ;
+}
+
+
+void disableTimer(uint8_t _timer)
+{
+  ;
+}
+
+
+void noTone(uint8_t _pin)
+{
+  ;
+}
+
