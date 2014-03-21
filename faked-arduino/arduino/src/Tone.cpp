da5533436769efb3faecaec85490cf601a8933c6
Henrik Sandklef <hesa@sandklef.com>
2014-03-21 10:07:22 +0100

    Added license (GPLv3)

--- faked-arduino/arduino/src/Tone.cpp
+++ faked-arduino/arduino/src/Tone.cpp
@@ -1,5 +1,26 @@
-/* 
- */
+/*****
+ *                                                                   
+ *               Searduino
+ *                      
+ *   Copyright (C) 2014 Henrik Sandklef 
+ *                                                                   
+ * This program is free software; you can redistribute it and/or     
+ * modify it under the terms of the GNU General Public License       
+ * as published by the Free Software Foundation; either version 3    
+ * of the License, or any later version.                             
+ *                                                                   
+ *                                                                   
+ * This program is distributed in the hope that it will be useful,   
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of    
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     
+ * GNU General Public License for more details.                      
+ *                                                                   
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software       
+ * Foundation, Inc., 51 Franklin Street, Boston,            
+ * MA  02110-1301, USA.                                              
+ ****/
+
 
 #include "Arduino.h"
 #include "searduino.h"
@@ -22,7 +43,7 @@ static tone_pin tone_pins[NR_TONE_PINS];
 static void init_tone_pins() ;
 void check_tone_pins() ;
 
-
+/* ------------------ */
 
 static int8_t toneBegin(uint8_t _pin)
 {
