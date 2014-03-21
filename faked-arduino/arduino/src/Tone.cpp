a3838c49b18c4c1d88854fcbe9399e21620ccaf9
Henrik Sandklef <hesa@sandklef.com>
2014-03-21 10:05:47 +0100

    First try - is working. Need more tests though

--- faked-arduino/arduino/src/Tone.cpp
+++ faked-arduino/arduino/src/Tone.cpp
@@ -4,6 +4,25 @@
 #include "Arduino.h"
 #include "searduino.h"
 
+#define NR_TONE_PINS 10
+typedef struct {
+  unsigned int   pin;
+  struct timeval stop_time;
+} tone_pin;
+
+static tone_pin tone_pins[NR_TONE_PINS];
+
+#define TONE_PIN_SET_INACTIVE(i)        tone_pins[i].pin = -1
+#define TONE_PIN_SET_ACTIVE(i, kingpin) tone_pins[i].pin = kingpin
+#define TONE_PIN_SET_STOP(i, ts)        tone_pins[i].stop_time = ts
+#define TONE_PIN(i)                     tone_pins[i].pin
+#define TONE_PIN_STOP_TIME(i)           tone_pins[i].stop_time
+
+/* internal functions */
+static void init_tone_pins() ;
+void check_tone_pins() ;
+
+
 
 static int8_t toneBegin(uint8_t _pin)
 {
@@ -11,12 +30,61 @@ static int8_t toneBegin(uint8_t _pin)
 }
 
 
-
-
 void tone(uint8_t _pin, unsigned int frequency, unsigned long duration)
 {
-  printf ("TONE OTNE:...\n");
-  ;
+  int i;
+  int pin;
+  struct timeval current_time;
+  init_tone_pins();
+  check_tone_pins();
+
+  for (i=0;i<NR_TONE_PINS;i++) 
+    {
+      // printf ("  find free pin entry at %d\n", i);
+      /* find first unused entry */
+      pin = TONE_PIN(i) ;
+      if (pin==_pin)  
+	{
+	  // printf ("  find earlier pin entry at %d YES!!!!!  ===> %d\n", i, TONE_PIN(i));
+	  break;
+	}
+      else if (pin==-1) 
+	{
+	  TONE_PIN_SET_ACTIVE(i, _pin);
+	  // printf ("  find free pin entry at %d YES!!!!!  ===> %d\n", i, TONE_PIN(i));
+	  break;
+	}
+    }
+  
+  if (i>=0 && i<(NR_TONE_PINS-1)) 
+    {
+      int unsigned long secs;
+      int unsigned long usecs;
+      int unsigned long sum_usecs;
+
+      gettimeofday(&current_time, NULL);
+      
+      usecs = current_time.tv_usec;
+      secs = current_time.tv_sec;
+
+      sum_usecs = usecs + duration*1000;
+      secs      = secs + sum_usecs/1000000;
+      usecs     = sum_usecs % 1000000;
+
+      // printf ("TIME: %ld %ld + %ld => \n      %ld %ld\n",
+      // 	      current_time.tv_sec, current_time.tv_usec,
+      // 	      duration*1000, 
+      // 	      secs, usecs);
+
+      current_time.tv_usec = usecs;
+      current_time.tv_sec  = secs;
+
+
+      
+      TONE_PIN_SET_STOP(i, current_time);
+
+    }
+  analogWrite(_pin, frequency);
 }
 
 
@@ -28,6 +96,81 @@ void disableTimer(uint8_t _timer)
 
 void noTone(uint8_t _pin)
 {
-  ;
+  int i;
+
+  for (i=0;i<NR_TONE_PINS;i++) 
+    {
+      if (_pin==TONE_PIN(i)) 
+	{
+	  TONE_PIN_SET_INACTIVE(i);
+	  analogWrite(_pin, 0);
+	}
+    }
 }
 
+
+static void init_tone_pins() 
+{
+  static int is_initialised=0;
+  int i ;
+
+  if (is_initialised!=0) 
+    {
+      return;
+    }
+
+  printf("INIT TONE PINS\n");
+
+  for (i=0;i<NR_TONE_PINS;i++) 
+    {
+      TONE_PIN_SET_INACTIVE(i);      
+    }
+  is_initialised=1;
+}
+
+static long has_timeout(struct timeval *curr_time, struct timeval *stop_time)
+{
+  long sec_diff;
+  long usec_diff;
+
+  sec_diff  =  curr_time->tv_sec  - stop_time->tv_sec   ;
+  usec_diff =  curr_time->tv_usec - stop_time->tv_usec  ;
+
+  // printf ("         DIFFS: %ld %ld\n", 
+  // 	  sec_diff, usec_diff);
+
+  if ( (sec_diff<0) || (usec_diff<0) )
+    {
+      return 0;
+    }
+
+  return 1;
+}
+
+void check_tone_pins() 
+{
+  int i ;
+  int pin;
+
+  for (i=0;i<NR_TONE_PINS;i++) 
+    {
+      pin = TONE_PIN(i) ;
+
+      if (pin!=-1) 
+	{
+	  struct timeval stop_time = TONE_PIN_STOP_TIME(i);
+	  struct timeval current_time ;
+	  gettimeofday(&current_time, NULL);
+
+	  if (has_timeout(&current_time, &stop_time) ) 
+	    {
+	      printf ("TIMEOUT.....on pin %d\n", TONE_PIN(i));
+	      
+	      /* Dont break the order of the two calls below */
+	      analogWrite(TONE_PIN(i), 0);
+	      TONE_PIN_SET_INACTIVE(i);
+	    }
+	  
+	}
+    }
+}
