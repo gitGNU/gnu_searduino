a60318e432fee7fc56a41f36ce1ad48e8de864e8
Henrik Sandklef <hesa@sandklef.com>
2014-04-11 11:57:23 +0200

    Setup script for vanilla deb

---
+++ bin/setup-debian.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DOC_PKGS=" pandoc " 
+
+DEVEL_PKGS="avrdude avr-gcc avr-g++  openjdk-6-jdk  arduino libtool autoconf automake gcc g++  python2.7-dev  autoconf-archive  python-dev  lib32z1-dev  check  gcc-avr binutils-avr avrdude avr-libc "
+
+my_install() 
+{
+    apt-get install -y  $*
+}
+
+
+my_install $DOC_PKGS
+
+my_install $DEVEL_PKGS
+
