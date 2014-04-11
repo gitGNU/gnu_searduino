2452c2e1481cc790305bcec3add4082c3deeac43
Henrik Sandklef <hesa@sandklef.com>
2014-04-11 21:26:15 +0200

    two arc supported

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+MACH=$(uname -m)
 
-DEFAULT_JAVA_PATH=/usr/lib/jvm/java-7-openjdk-amd64/include/
+if [ "$MACH" = "i686" ]
+then
+    ARCH=i386
+else
+    ARCH=amd64
+fi
+
+DEFAULT_JAVA_PATH=/usr/lib/jvm/java-7-openjdk-$ARCH/include/
 export CFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
 export CXXFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
+
 
 make -f Makefile.git && \
   ./configure --enable-unittest --prefix=/opt/searduino --enable-python-extension && \
