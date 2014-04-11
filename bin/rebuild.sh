797448bca01a8261f1bb07a0c582e6e625b1b13a
Henrik Sandklef <hesa@sandklef.com>
2014-04-11 21:20:32 +0200

    first rev

---
+++ bin/rebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+DEFAULT_JAVA_PATH=/usr/lib/jvm/java-7-openjdk-amd64/include/
+export CFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
+export CXXFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
+
+make -f Makefile.git && \
+  ./configure --enable-unittest --prefix=/opt/searduino --enable-python-extension && \
+  make clean && make && \
+  sudo make install &&  \
+  make check &&         \
+  echo "Wow, it all passed :)"
+
+
+
