2d2039bacae9a29b3484a17be8ff70c608ec73f9
Henrik Sandklef <hesa@sandklef.com>
2014-04-12 22:43:58 +0200

    ubuntu specifics

---
+++ bin/setup-ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DOC_PKGS=" pandoc " 
+# python2.7-dev python-dev
+DEVEL_PKGS="avrdude gcc gcc-avr  openjdk-7-jdk  arduino libtool autoconf automake gcc g++    autoconf-archive      check binutils-avr avrdude avr-libc libtool"
+
+my_install() 
+{
+    apt-get install -y  $*
+}
+
+
+my_install $DOC_PKGS
+
+my_install $DEVEL_PKGS
+
