c618aec766478c0f1d529a60859bae9bbc4e1539
Henrik Sandklef <hesa@sandklef.com>
2014-04-12 22:44:31 +0200

    Bumping down openjdk to 6 Moving ubuntu stuff to separate file

--- bin/setup-debian.sh
+++ bin/setup-debian.sh
@@ -2,7 +2,7 @@
 
 DOC_PKGS=" pandoc " 
 # python2.7-dev python-dev
-DEVEL_PKGS="avrdude gcc gcc-avr  openjdk-7-jdk  arduino libtool autoconf automake gcc g++    autoconf-archive      check binutils-avr avrdude avr-libc libtool"
+DEVEL_PKGS="avrdude gcc gcc-avr  openjdk-6-jdk  arduino libtool autoconf automake gcc g++    autoconf-archive      check binutils-avr avrdude avr-libc libtool"
 
 my_install() 
 {
