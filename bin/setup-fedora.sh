744301ae7ab9045b15f32a257fa551bfdf834583
Henrik Sandklef <hesa@sandklef.com>
2014-04-13 22:44:08 +0200

    java devel pkg fix

--- bin/setup-fedora.sh
+++ bin/setup-fedora.sh
@@ -2,7 +2,7 @@
 
 DOC_PKGS=" pandoc " 
 
-DEVEL_PKGS="java-1.7.0-openjdk.i686-devel avr-binutils avr-gcc avr-gcc-g++ avr-libc avrdude automake autoconf libtool gcc-c++ "
+DEVEL_PKGS=" java-1.7.0-openjdk-devel  avr-binutils avr-gcc avr-gcc-g++ avr-libc avrdude automake autoconf libtool gcc-c++ "
 
 my_install() 
 {
