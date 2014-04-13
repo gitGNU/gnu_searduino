5f5e1ac9cc14229d953548851df23e27efba9e20
Henrik Sandklef <hesa@sandklef.com>
2014-04-13 22:58:20 +0200

    Build frmo Fedora's Arduino sources

--- bin/build-fedora.sh
+++ bin/build-fedora.sh
@@ -11,12 +11,19 @@ fi
 
 DEFAULT_JAVA_PATH=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.60-2.4.5.0.fc19.$ARCH/include/
 
+
+if [ "$1" = "fedora" ]
+then
+    FEDORA_ARGS="--enable-fedora-sources"
+fi
+
+
 export CFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
 export CXXFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
 
 #--enable-unittest
 make -f Makefile.git && \
-  ./configure  --prefix=/opt/searduino --disable-python-extension  --disable-pearduino && \
+  ./configure  --prefix=/opt/searduino --disable-python-extension  --disable-pearduino $FEDORA_ARGS  && \
   make clean && make && \
   sudo make install && \
   echo "Wow, it all passed :)"
