d92bfc9a1cace6304abdf94464037aeb755b0a6a
Henrik Sandklef <hesa@sandklef.com>
2014-04-13 11:53:14 +0200

    More build files for VMM builds

---
+++ bin/build-ubuntu.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+MACH=$(uname -m)
+
+if [ "$MACH" = "i686" ]
+then
+    ARCH=i386
+else
+    ARCH=amd64
+fi
+
+if [ "$1" = "debian" ]
+then
+    DEBIAN_ARGS="--enable-debian-sources"
+else
+    DEBIAN_ARGS=""
+fi
+
+DEFAULT_JAVA_PATH=/usr/lib/jvm/java-7-openjdk-$ARCH/include/
+
+export CFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
+export CXXFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
+
+#--enable-unittest
+make -f Makefile.git && \
+  ./configure  --prefix=/opt/searduino --disable-python-extension  --disable-pearduino $DEBIAN_ARGS && \
+  make clean && make && \
+  sudo make install && \
+  echo "Wow, it all passed :)"
+
+#&&  \
+#  make check &&         \
+
+
+
