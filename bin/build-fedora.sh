ce14d1c7181d2e27b2e09285e63a0b1319577f83
Henrik Sandklef <hesa@sandklef.com>
2014-04-13 14:31:34 +0200

    first rev

---
+++ bin/build-fedora.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+MACH=$(uname -m)
+
+if [ "$MACH" = "i686" ]
+then
+    ARCH=i386
+else
+    ARCH=amd64
+fi
+
+DEFAULT_JAVA_PATH=/usr/lib/jvm/java-7-openjdk-$ARCH/include/
+
+export CFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
+export CXXFLAGS="-I${DEFAULT_JAVA_PATH} -I${DEFAULT_JAVA_PATH}/linux/"
+
+#--enable-unittest
+make -f Makefile.git && \
+  ./configure  --prefix=/opt/searduino --disable-python-extension  --disable-pearduino && \
+  make clean && make && \
+  sudo make install && \
+  echo "Wow, it all passed :)"
+
+#&&  \
+#  make check &&         \
+
+
+
