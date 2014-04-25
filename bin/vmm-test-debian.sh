868534cc6ed89dd4d4a2ac9d62e56cc821e99146
Henrik Sandklef <hesa@sandklef.com>
2014-04-25 10:37:41 +0200

    new structure

--- bin/vmm-test-debian.sh
+++ bin/vmm-test-debian.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-
 VMM=/opt/bin/tm-vmm
 CLIENT=Debian-6-0-6
 SNAPSHOT_NAME=SEARD_BASE
 DIST="debian"
+
+
+FUNC=$(dirname $0)/vmm-test-functions
+if [ -f $FUNC ]
+then
+    . $FUNC
+else
+    echo "Failed finding functions file ....."
+    exit 1
+fi
+
+vmm_test  $*
 
 
