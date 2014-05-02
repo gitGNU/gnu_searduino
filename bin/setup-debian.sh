0bc33f90a4805039bad76c6ef4802833ac325938
Henrik Sandklef <hesa@sandklef.com>
2014-05-02 12:16:03 +0200

    doc deps added

--- bin/setup-debian.sh
+++ bin/setup-debian.sh
@@ -2,7 +2,7 @@
 
 DOC_PKGS=" pandoc " 
 # python2.7-dev python-dev
-DEVEL_PKGS="avrdude gcc gcc-avr  openjdk-6-jdk  arduino libtool autoconf automake gcc g++    autoconf-archive      check binutils-avr avrdude avr-libc libtool pkg-config  libxtst-dev  python-dev zlib1g-dev"
+DEVEL_PKGS="avrdude gcc gcc-avr  openjdk-6-jdk  arduino libtool autoconf automake gcc g++    autoconf-archive      check binutils-avr avrdude avr-libc libtool pkg-config  libxtst-dev  python-dev zlib1g-dev texi2html  texlive-extra-utils texlive-font-utils doxygen texinfo"
 
 my_install() 
 {
