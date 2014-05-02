dde2615c2241691071ae78a8b763387380d2dba3
Henrik Sandklef <hesa@sandklef.com>
2014-05-02 12:03:02 +0200

    Lib deps for python

--- bin/setup-debian.sh
+++ bin/setup-debian.sh
@@ -2,7 +2,7 @@
 
 DOC_PKGS=" pandoc " 
 # python2.7-dev python-dev
-DEVEL_PKGS="avrdude gcc gcc-avr  openjdk-6-jdk  arduino libtool autoconf automake gcc g++    autoconf-archive      check binutils-avr avrdude avr-libc libtool pkg-config  libxtst-dev"
+DEVEL_PKGS="avrdude gcc gcc-avr  openjdk-6-jdk  arduino libtool autoconf automake gcc g++    autoconf-archive      check binutils-avr avrdude avr-libc libtool pkg-config  libxtst-dev  python-dev zlib1g-dev"
 
 my_install() 
 {
