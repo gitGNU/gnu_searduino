b8d08d02b3c138dff01e7cb1bd8448a17c428c80
Henrik Sandklef <hesa@sandklef.com>
2014-05-04 12:04:50 +0200

    First rev

---
+++ bin/build-darwin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash                                                                                         
+
+DIST=darwin
+
+FUNC=$(dirname $0)/build-functions
+if [ -f $FUNC ]
+then
+    . $FUNC
+else
+    echo "Failed finding functions file ....."
+    exit 1
+fi
+
+dobuild $*
+
+
+
