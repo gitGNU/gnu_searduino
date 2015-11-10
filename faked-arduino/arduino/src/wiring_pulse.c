5cd4705194a9b39d04083d743bc16c47c3a3a35f
Henrik Sandklef <hesa@sandklef.com>
2015-11-10 23:49:26 +0100

    pulseIn now has an impl

--- faked-arduino/arduino/src/wiring_pulse.c
+++ faked-arduino/arduino/src/wiring_pulse.c
@@ -22,11 +22,48 @@
  ****/
 
 #include "wiring_private.h"
+#include "arduino/pins_arduino.h"
+#include "arduino/searduino_pin.h"
 #include "pins_arduino.h"
 #include "utils/print.h"
+#include "arduino/setup.h"
+#include "../include/arduino/error.h"
+#include "utils/print.h"
+#include "utils/types.h"
+#include "arduino/error.h"
+#include "utils/error.h"
 
 unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
 {
-  PRINT_DUMMY_FUNCTION_IMPLEMENTATION();
-  return 100; 
+  int ret_val;
+
+  set_generic_pin_type(pin, SEARDUINO_PIN_TYPE_ANALOG);
+
+
+  searduino_setup();
+  if (PIN_OUT_OF_RANGE(pin))
+    {
+      SEARD_ERROR(SEARD_ARDUINO_OUT_OF_BOUND);
+      return 0;
+    }
+  
+  /* printf ("wiring: analogRead(%d) => %d\n",  */
+  /* 	  pin, get_analog_pin_val(pin)); */
+  /* printf ("analogRead(%d)=>%d\n", pin, get_analog_pin_val(pin) ); */
+  /* printf ("analogRead(%d)=>%d\n", pin, get_generic_pin_val(pin, SEARDUINO_PIN_TYPE_ANALOG)); */
+  
+  ret_val = get_analog_pin_val(pin);
+
+  if (ret_val < 0 ) 
+    {
+      log_error("Analog value to return to analogRead is less than 0, returning 0");
+      ret_val = 0 ;
+    }
+  else if (ret_val > 1024) 
+    {
+      log_error("Analog value to return to analogRead is higher than 1024, returning 1024");
+      ret_val = 1024 ;
+    }
+
+  return ret_val;
 }
