19c253eb71e8b85f152556d8fc8aa423e7e83de6
Henrik Sandklef <hesa@sandklef.com>
2015-12-25 18:52:49 +0100

    env vars needed to build

--- bin/build-cygwin.sh
+++ bin/build-cygwin.sh
@@ -5,6 +5,18 @@ cat configure.ac.orig | \
   sed -e 's,AM_MISSING_PROG,#AM_MISSING_PROG,g' \
       -e 's,AC_PYTHON_DEVEL,#AC_PYTHON_DEVEL,g' > configure.ac 
 
+export JAVA_HOME=/cygdrive/c/Java/jdk1.7.0_79
+export PATH=$PATH:/cygdrive/c/Java/jdk1.7.0_79/bin
+
+
+
+
+
+CFLAGS="-I${JAVA_HOME}/include -D__int64=int64_t"
+CPPFLAGS="$CFLAGS"
+
+
 make -f Makefile.git && \
-  ./configure --disable-python --disable-unittest --prefix=/opt/searduino && \
+  ./configure CFLAGS="$CFLAGS" CPPFLAGS="$CFLAGS"  --disable-unittest --disable-python --disable-unittest --prefix=/opt/searduino && \
   make && make install
+
